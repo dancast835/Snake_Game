d6cd327956365e8de39ddcb7d83ec025e6edd9f0
Daniel castro <dancast23@MacBookPro.lan>
2025-04-30 11:17:16 -0400

    Initial commit

---
+++ scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(-100,275)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.high_as_str = str(self.high_score)
+        with open("data.txt", mode="w") as data:
+            data.write(self.high_as_str)
+        self.score = 0
+        self.update_scoreboard()
